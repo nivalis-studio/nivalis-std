147cf18d1385d3fde49c55961c478025ccf9da8c
pnodet <paul.nodet@gmail.com>
2024-01-16 00:21:32 +0800

    feat: add currency types and formatter

---
+++ src/currency/index.ts
@@ -0,0 +1,23 @@
+import type {CurrencyCode} from './types';
+export type {CurrencyCode};
+
+type Money = {
+	amount: string;
+	currencyCode: CurrencyCode;
+};
+
+export const formatPrice = (
+	{amount, currencyCode}: Money,
+	locale = 'fr',
+	quantity = 1,
+	disableCents = false,
+): string => {
+	const price = Number.parseFloat(amount);
+
+	return new Intl.NumberFormat(locale, {
+		style: 'currency',
+		currency: currencyCode,
+		maximumFractionDigits: disableCents && price % 1 === 0 ? 0 : 2,
+		minimumFractionDigits: disableCents && price % 1 === 0 ? 0 : 2,
+	}).format(price * quantity);
+};
