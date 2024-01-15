68e933100bb72e26d55121de7e9e0751e13a33b4
pnodet <paul.nodet@gmail.com>
2024-01-16 00:20:05 +0800

    feat: add future

---
+++ src/future/index.ts
@@ -0,0 +1,21 @@
+/**
+ * Just like a Promise but you can manually resolve or reject it
+ */
+export class Future<T> {
+	#resolve: (value: T) => void;
+	#reject: (value: unknown) => void;
+	promise: Promise<T>;
+
+	constructor() {
+		this.promise = new Promise((subresolve, subreject) => {
+			this.#resolve = subresolve;
+			this.#reject = subreject;
+		});
+	}
+	get resolve(): (value: T) => void {
+		return this.#resolve;
+	}
+	get reject(): (error: unknown) => void {
+		return this.#reject;
+	}
+}
