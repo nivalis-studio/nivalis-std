455ec3ec1f4ca15d4899946cc1ec3df8dea0e981
pnodet <paul.nodet@gmail.com>
2024-01-16 00:21:46 +0800

    feat: add basic discord error parser

---
+++ src/exceptions/to-discord.ts
@@ -0,0 +1,40 @@
+export const toDiscord = (err: {
+	traceId: string;
+	cause: {message: string};
+	name: string;
+	stack: string | string[];
+	status: string;
+	message: string;
+}) => {
+	return [
+		{
+			color: 16_711_680, // Red color
+			fields: [
+				{
+					inline: true,
+					name: 'Trace ID',
+					value: err.traceId,
+				},
+				{
+					inline: true,
+					name: 'Cause',
+					value: err.cause ? err.cause.message : 'N/A',
+				},
+				{
+					inline: true,
+					name: 'Name',
+					value: err.name,
+				},
+				{
+					inline: false,
+					name: 'Stack Trace',
+					value: `\`\`\`${err.stack ? err.stack.slice(0, 1014) : 'N/A'}\`\`\``,
+				},
+			],
+			footer: {
+				text: `Logged at: ${new Date().toISOString()}`,
+			},
+			title: `[${err.status}] ${err.message}`,
+		},
+	];
+};
