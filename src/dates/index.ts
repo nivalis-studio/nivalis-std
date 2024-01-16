b1aaf341a09848164b55885d084b125eada29b9c
pnodet <paul.nodet@gmail.com>
2024-01-16 23:25:10 +0800

    fix: update date utils

--- src/dates/index.ts
+++ src/dates/index.ts
@@ -1,7 +1,7 @@
-import {DAY_IN_MS} from '../constants';
+import {MS_IN_DAY} from '../constants';
 
 export const getDaysSince = (date: Date) =>
-	Math.floor((Date.now() - date.getTime()) / DAY_IN_MS);
+	Math.floor((Date.now() - date.getTime()) / MS_IN_DAY);
 
 export const isToday = (date: Date) =>
 	new Date().toDateString() === date.toDateString();
@@ -10,7 +10,7 @@ export const isInThisMonth = (date: Date, month: number, year: number) =>
 	date.getMonth() === month && date.getFullYear() === year;
 
 export const getDateFromDeltaDays = (date: Date, numberDays: number) =>
-	new Date(date.valueOf() + DAY_IN_MS * numberDays);
+	new Date(date.valueOf() + MS_IN_DAY * numberDays);
 
 export const convertToParisTime = (dateAsUtc: Date) =>
 	new Date(dateAsUtc.getTime() + dateAsUtc.getTimezoneOffset() * 60 * 1000);
