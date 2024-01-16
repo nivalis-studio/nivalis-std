ee81cd4152f4e0da1f4467565e3071112a502b4e
pnodet <paul.nodet@gmail.com>
2024-01-16 19:42:04 +0800

    feat: add dates utils

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,2 @@
 export const EARTH_RADIUS_IN_METERS = 6_378_137;
+export const DAY_IN_MS = 86_400_000;
--- src/dates/index.ts
+++ src/dates/index.ts
@@ -1,2 +1,16 @@
+import {DAY_IN_MS} from '../constants';
+
 export const getDaysSince = (date: Date) =>
-	Math.floor((Date.now() - date.getTime()) / 1000 / 60 / 60 / 24);
+	Math.floor((Date.now() - date.getTime()) / DAY_IN_MS);
+
+export const isToday = (date: Date) =>
+	new Date().toDateString() === date.toDateString();
+
+export const isInThisMonth = (date: Date, month: number, year: number) =>
+	date.getMonth() === month && date.getFullYear() === year;
+
+export const getDateFromDeltaDays = (date: Date, numberDays: number) =>
+	new Date(date.valueOf() + DAY_IN_MS * numberDays);
+
+export const convertToParisTime = (dateAsUtc: Date) =>
+	new Date(dateAsUtc.getTime() + dateAsUtc.getTimezoneOffset() * 60 * 1000);
