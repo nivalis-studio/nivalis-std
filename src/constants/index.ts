8a5c46d14d7251ee61df90900d250cd1b73cf452
pnodet <paul.nodet@gmail.com>
2024-01-16 23:24:18 +0800

    feat: add math utils functions

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,51 @@
 export const EARTH_RADIUS_IN_METERS = 6_378_137;
-export const DAY_IN_MS = 86_400_000;
+
+export const MS_IN_SECOND = 1_000;
+export const MS_IN_MINUTE = 60_000;
+export const MS_IN_HOUR = 3_600_000;
+export const MS_IN_DAY = 86_400_000;
+export const MS_IN_WEEK = 604_800_000;
+
+export const SECONDS_IN_MINUTE = 60;
+export const SECONDS_IN_HOUR = 3600;
+export const SECONDS_IN_DAY = 86_400;
+export const SECONDS_IN_WEEK = 604_800;
+
+export const MINUTES_IN_HOUR = 60;
+export const MINUTES_IN_DAY = 1440;
+export const MINUTES_IN_WEEK = 10_080;
+
+export const HOURS_IN_DAY = 24;
+export const HOURS_IN_WEEK = 168;
+
+/**
+ * ```tex
+ * π / 180.0
+ * ```
+ */
+export const PI_DIV_180 = 1.7453292519943295e-2;
+
+/**
+ * ```tex
+ * 180.0 / π
+ * ```
+ */
+export const CONST_180_DIV_PI = 57.29577951308232;
+
+/**
+ * Maximum length of a generic array.
+ *
+ * ```tex
+ * 2^{32} - 1
+ * ```
+ */
+export const MAX_ARRAY_LENGTH = 4294967295 >>> 0;
+
+/**
+ * Maximum length of a typed array.
+ *
+ * ```tex
+ * 2^{53} - 1
+ * ```
+ */
+export const MAX_TYPED_ARRAY_LENGTH = 9007199254740991;
--- src/math/index.ts
+++ src/math/index.ts
@@ -1,14 +1,84 @@
-export const squared = (x: number) => x * x;
-export const hav = (x: number) => squared(Math.sin(x / 2));
-export const toRad = (x: number) => (x * Math.PI) / 180;
+import {CONST_180_DIV_PI, PI_DIV_180} from '../constants';
 
 /**
  * Round a number to a chosen precision.
- * @param number
- * @param precision
+ * @param {number} x - input value
+ * @param {number} precision
  * @returns rounded number
  */
-export const roundNumber = (number: number, precision: number): number => {
+export const roundNumber = (x: number, precision: number): number => {
 	const factor = 10 ** precision;
-	return Math.round(number * factor) / factor;
+	return Math.round(x * factor) / factor;
 };
+
+/**
+ * Computes the half-value versed sine.
+ *
+ * @param {number} x - input value (in radians)
+ * @returns {number} half-value versed sine
+ *
+ * @example
+ * var v = haversin( 3.141592653589793/2.0 );
+ * // returns ~0.5
+ */
+export const haversin = (x: number): number => (1.0 - Math.cos(x)) / 2.0;
+
+/**
+ * Computes the half-value versed cosine.
+ *
+ * @param {number} x - input value (in radians)
+ * @returns {number} half-value versed cosine
+ *
+ * @example
+ * var v = havercos( 3.141592653589793/2.0 );
+ * // returns 0.5
+ */
+export const havercos = (x: number): number => (1.0 + Math.cos(x)) / 2.0;
+
+/**
+ * Converts an angle from degrees to radians.
+ *
+ * @param {number} x - angle in degrees
+ * @returns {number} angle in radians
+ *
+ * @example
+ * var r = deg2rad( 90.0 );
+ * // returns ~1.571
+ *
+ * @example
+ * var r = deg2rad( -45.0 );
+ * // returns ~-0.785
+ */
+export const deg2rad = (x: number): number => x * PI_DIV_180;
+
+/**
+ * Converts an angle from radians to degrees.
+ *
+ * @param {number} x - angle in radians
+ * @returns {number} angle in degrees
+ *
+ * @example
+ * var d = rad2deg( 3.141592653589793/2.0 );
+ * // returns 90.0
+ *
+ * @example
+ * var d = rad2deg( -3.141592653589793/4.0 );
+ * // returns -45.0
+ */
+export const rad2deg = (x: number): number => x * CONST_180_DIV_PI;
+
+/**
+ * Computes the absolute value of `x`.
+ *
+ * @param {number} x - input value
+ * @returns {number} absolute value
+ *
+ * @example
+ * var v = abs( -1.0 );
+ * // returns 1.0
+ *
+ * @example
+ * var v = abs( 2.0 );
+ * // returns 2.0
+ */
+export const abs = (x: number): number => (x < 0.0 ? -x : x);
