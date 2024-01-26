4fc5fe3b8617b4c0e3e456236382605b7b5b8a51
pnodet <paul.nodet@gmail.com>
2024-01-26 19:31:33 +0800

    fix: add to lowercase in capitalize

--- src/strings/capitalize.ts
+++ src/strings/capitalize.ts
@@ -2,7 +2,7 @@
  * Capitalize the first letter of a string.
  */
 export const capitalize = (string: string): string =>
-	string.charAt(0).toUpperCase() + string.slice(1);
+	string.charAt(0).toUpperCase() + string.toLowerCase().slice(1);
 
 /**
  * Capitalize the first letter of each word in a string.
