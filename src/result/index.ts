b1b4a99831c330cfd0aa9afdc4cfe63ac1e1dd82
pnodet <paul.nodet@gmail.com>
2024-01-26 13:11:52 +0800

    feat(result): add valuesFromResults util

--- src/result/index.ts
+++ src/result/index.ts
@@ -1,3 +1,4 @@
+import {nonNullable} from '../non-nullable';
 import type {Result} from './types';
 
 export {Err, err} from './err';
@@ -6,3 +7,7 @@ export type {Result} from './types';
 export {wrap, wrapSync} from './catch';
 
 export const unwrap = <R, E>(result: Result<R, E>): R => result.unwrap();
+export const valuesFromResults = <R, E>(results: Result<R, E>[]): R[] =>
+	results
+		.map((result) => (result.isOk() ? result.value : null))
+		.filter(nonNullable);
