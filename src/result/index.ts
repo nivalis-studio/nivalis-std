9fa2786cbd5d499226168df87f514272e161c44a
pnodet <paul.nodet@gmail.com>
2024-01-26 13:11:40 +0800

    feat(result): add unwrap util

--- src/result/index.ts
+++ src/result/index.ts
@@ -1,4 +1,8 @@
+import type {Result} from './types';
+
 export {Err, err} from './err';
 export {Ok, ok} from './ok';
 export type {Result} from './types';
 export {wrap, wrapSync} from './catch';
+
+export const unwrap = <R, E>(result: Result<R, E>): R => result.unwrap();
