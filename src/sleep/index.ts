c611bcd63d4553964736755ce760818adbef90f0
pnodet <paul.nodet@gmail.com>
2024-07-15 14:59:37 +0200

    feat: add abort signal

--- src/sleep/index.ts
+++ src/sleep/index.ts
@@ -1,7 +1,28 @@
 /**
  * Sleep for a given amount of time
  */
-export const sleep = async (ms: number) =>
-	new Promise((resolve) => {
-		setTimeout(resolve, ms);
+export const sleep = async (
+	ms: number,
+	{
+		signal,
+	}: {
+		signal?: AbortSignal;
+	},
+) =>
+	new Promise((resolve, reject) => {
+		if (signal?.aborted) {
+			reject(new Error('Aborted'));
+			return;
+		}
+
+		const timeout = setTimeout(resolve, ms);
+
+		const onAbort = () => {
+			clearTimeout(timeout);
+			reject(new Error('Aborted'));
+		};
+
+		if (signal) {
+			signal.addEventListener('abort', onAbort, { once: true });
+		}
 	});
